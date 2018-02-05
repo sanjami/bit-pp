3870e413b5275100bd650aaee1c1198a004cc274
BIT student <classroom@bgit.rs>
2018-02-05 18:02:38 +0100

    commitnig arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,66 @@
+var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'];
+console.log(month[5],month[9],month[0]);
+
+var day = ['Mon', 'Thu', 'Wen', 'Thr', 'Fri', 'Sat', 'Sun'];
+console.log(day[6]);
+
+var n = [2,-4,5,-2,-11];
+var negative=[];
+if (n[0]<0){
+negative[0]=n[0];
+}
+if (n[1]<0){
+    negative[1]=n[1];
+}
+if (n[2]<0){
+    negative[2]=n[2];
+}
+if (n[3]<0){
+    negative[3]=n[3];
+}
+if (n[4]<0){
+    negative[4]=n[4];
+}
+console.log(negative);
+
+
+var n = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+var divide=[];
+if (n[0]%3==0){
+    divide[0]=n[0];
+}
+if (n[1]%3==0){
+    divide[1]=n[1];
+}
+
+if (n[2]%3==0){
+    divide[2]=n[2];
+}
+
+if (n[3]%3==0){
+    divide[3]=n[3];
+}
+
+if (n[4]%3==0){
+    divide[4]=n[4];
+}
+
+if (n[5]%3==0){
+    divide[5]=n[5];
+}
+
+if (n[6]%3==0){
+    divide[6]=n[6];
+}
+
+if (n[7]%3==0){
+    divide[7]=n[7];
+}
+
+if (n[8]%3==0){
+    divide[8]=n[8];
+}
+
+
+console.log(divide);
+    
