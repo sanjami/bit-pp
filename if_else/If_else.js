51bedcccbb449e2883f5dd2bd242a3bf5c19bac1
BIT student <classroom@bgit.rs>
2018-02-05 15:13:15 +0100

    if_else commit

---
+++ if_else/If_else.js
@@ -0,0 +1,19 @@
+var p1 = 14, p2 = 50, p3 = 20;
+
+
+    if(p1<p2 && p1<p3)
+    {
+        console.log(1+p2+p3)
+        console.log(1+p2+p3)
+
+    }
+    else if(p2<p1 && p2<p3){
+        console.log(1+p1+p3)
+        console.log(1+p2+p3)
+    }
+    else if(p3<p1 && p3<p2){
+        console.log(1+p1+p2)
+        console.log(1+p2+p3)
+    }
+    
+
