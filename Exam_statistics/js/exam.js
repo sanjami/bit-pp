86e363a4c5f70d335c821d027aacaf980b4c4a7c
sanja <sanjam23@gmail.com>
2018-03-04 23:20:47 +0100

    Exam statistic before description

--- Exam_statistics/js/exam.js
+++ Exam_statistics/js/exam.js
@@ -1,15 +1,47 @@
-var totalStudent = 0;
-var totalPassed = 0;
-var totalFailed = 0;
+var totalStudent1 = [0, 0];
+var totalStudent2 = [0, 0];
+var totalStudent3 = [0, 0];
+var totalStudent;
+var percent;
+
+var subjectName;
+
+var subjectIndexSt;
 
 document.querySelector("#subject").addEventListener("change", function (event) {
+
+    subjectIndexSt = document.querySelector("#subject").value;
+    var subjectIndexNum = parseInt(subjectIndexSt);
+
+    switch (subjectIndexNum) {
+        case 1:
+            subjectName = 'WEB';
+            totalStudent = totalStudent1;
+            break;
+        case 2:
+            subjectName = 'JavaScript';
+            totalStudent = totalStudent2;
+            break;
+        case 3:
+            subjectName = "SPA";
+            totalStudent = totalStudent3;
+            break;
+    };
+
     var allOptions = document.querySelectorAll(".resultList");
     allOptions.forEach(function (element) {
         element.classList.add("hiden")
     });
+
     var resultList = document.querySelector(".list" + this.value);
     resultList.classList.toggle("hiden");
+    percent = parseInt(totalStudent[1] / (totalStudent[0] + totalStudent[1]) * 100);
+    document.querySelector("#passedNum").textContent = totalStudent[0];
+    document.querySelector("#failedNum").textContent = totalStudent[1];
+    document.querySelector("#failedPercent").textContent = percent + '%';
+    document.querySelector("#total").textContent = ' ' + (totalStudent[0] + totalStudent[1]);
 })
+
 
 
 document.querySelector(".btn").addEventListener("click", inputData);
@@ -20,25 +52,7 @@ document.querySelector("body").addEventListener("keydown", function (event) {
 })
 
 
-
 function inputData() {
-
-    var subjectIndexSt = document.querySelector("#subject").value;
-    var subjectIndexNum = parseInt(subjectIndexSt);
-
-    var subjectName;
-
-    switch (subjectIndexNum) {
-        case 1:
-            subjectName = 'WEB';
-            break;
-        case 2:
-            subjectName = 'JavaScript';
-            break;
-        case 3:
-            subjectName = "SPA";
-            break;
-    };
 
     var subjectObject = new Subject(subjectName);
 
@@ -50,20 +64,31 @@ function inputData() {
     function validation(studentName, studentGrade) {
         if (studentName == '' || studentGrade == '') {
             return "All data is required"
+        } else if (studentGrade > 10 || studentGrade < 1) {
+            return "Grade have to be between 1 and 10";
+        } else if (studentName.indexOf(' ') == -1) {
+            return 'Name and surname are required';
         } else {
-            return "OK"
+            return 'Ok';
         }
     }
     result = validation(studentName, studentGrade);
 
-    if (result == 'All data is required') {
+    if (result != 'Ok') {
         var text = document.createTextNode(result);
         var errorDiv = document.querySelector("#errorText");
         errorDiv.appendChild(text);
     } else {
 
-        var student = new CreateStudent(studentName, studentGrade);
+        position = studentName.indexOf(' ');
+        console.log(position);
+        var name = studentName.slice(0, position);
+        var nameUp = name.charAt(0).toUpperCase() + name.slice(1, name.length);
+        var surname = studentName.slice(position + 1, studentName.length);
+        var surnameUp = surname.charAt(0).toUpperCase() + surname.slice(1, surname.length);
 
+        var fixedStudentName = nameUp + ' ' + surnameUp;
+        var student = new CreateStudent(fixedStudentName, studentGrade);
 
         subjectObject.addStudent(student);
 
@@ -77,23 +102,19 @@ function inputData() {
         liStudent.appendChild(textLi);
 
 
-        totalStudent++;
-
         if (studentResult == 'passed') {
             passedList.appendChild(liStudent);
-            totalPassed++
+            totalStudent[0] = totalStudent[0] + 1;
         } else {
             failedList.appendChild(liStudent)
-            totalFailed++
+            totalStudent[1] = totalStudent[1] + 1;
         };
 
-
-        var percent = parseInt(totalFailed / totalStudent * 100);
-
-        document.querySelector("#passedNum").textContent = totalPassed;
-        document.querySelector("#failedNum").textContent = totalFailed;
+        percent = parseInt(totalStudent[1] / (totalStudent[0] + totalStudent[1]) * 100);
+        document.querySelector("#passedNum").textContent = totalStudent[0];
+        document.querySelector("#failedNum").textContent = totalStudent[1];
         document.querySelector("#failedPercent").textContent = percent + '%';
-        document.querySelector("#total").textContent = ' ' + totalStudent;
+        document.querySelector("#total").textContent = ' ' + (totalStudent[0] + totalStudent[1]);
 
         nameInput.value = '';
         gradeInput.value = '';
