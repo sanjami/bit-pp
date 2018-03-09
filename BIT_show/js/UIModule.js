ff89d9689af7d6385cf26569d77d821bdcf40b7c
sanja <sanjam23@gmail.com>
2018-03-10 00:22:10 +0100

    Working on bit show

--- BIT_show/js/UIModule.js
+++ BIT_show/js/UIModule.js
@@ -1,15 +1,17 @@
-let UIModule = (function(){
+let UIModule = (function () {
 
     // uzeti selektor
     const selectors = {
-        searchBox : '#search-box',
-        movieContainer : '#movie_container'
+        searchBox: '#search-box',
+        movieContainer: '#movie_container',
+        oneMovieContainer: '#oneshow_container'
     }
 
     // selektovano polje
 
     const searchInput = $(selectors.searchBox);
     const movieContainerDiv = $(selectors.movieContainer);
+    const oneMovieContainerDiv = $(selectors.oneMovieContainer)
     // uzeti vrednost iz search-a
 
     let getDataSearh = () => {
@@ -22,25 +24,45 @@ let UIModule = (function(){
     // pakovanje filmova u div
 
     function makeListOfMovies(showLists) {
-        console.log(showLists);
-       
-       showLists.forEach(element => {
-        let movieDiv = $('<div>');
-        let imgTag = $('<img>');
-        let linkTag = $('<a>');
-           imgTag.attr('src',element.image);
-           linkTag.attr('href', `showInfoPage.html/?${element.id}`);
-           linkTag.text(element.name);
 
-           movieDiv.append(imgTag);
-           movieDiv.append(linkTag);
-           movieContainerDiv.append(movieDiv);
-       });
+        showLists.forEach(element => {
+            let movieDiv = $('<div>');
+            let imgTag = $('<img>');
+            imgTag.css({ 'width': '100%', 'margin-top': '10%' });
+            let linkTag = $('<a>');
+            imgTag.attr('src', element.image);
+            linkTag.attr('href', '#');
+            linkTag.attr('id', element.id);
+            linkTag.addClass('clickLink');
+            linkTag.text(element.name);
+
+            movieDiv.append(imgTag);
+            movieDiv.append(linkTag);
+            movieDiv.attr('class', 'col-4');
+            movieContainerDiv.append(movieDiv);
+        });
 
     }
 
-return {
-    makeListOfMovies : makeListOfMovies
-}
+    function showMovieInfo(show) {
+
+        $('h1').text(show.name);
+        let imgDiv = $('div');
+        imgDiv.addClass('col-6');
+        let imgTag = $('<img>');
+        imgTag.attr('src', show.image);
+        imgDiv.append(imgTag);
+
+        
+        oneMovieContainerDiv.append(imgDiv);
+
+    }
+
+
+
+    return {
+        makeListOfMovies: makeListOfMovies,
+        showMovieInfo: showMovieInfo
+    }
 
 })();
--- BIT_show/js/mainModule.js
+++ BIT_show/js/mainModule.js
@@ -1,28 +1,67 @@
-const mainModule = (function(dataModule, UIModule){
+const mainModule = (function (dataModule, UIModule) {
 
-$(function(){
+    var tvShows = new dataModule.TvShows();
 
     let request = $.ajax({
-
         url: 'http://api.tvmaze.com/shows',
         method: 'GET'
 
     })
 
-    request.done(function(msg){
+    request.done(function (msg) {
         let firsFifty = msg.slice(0, 50);
-        let tvShows = new dataModule.TvShows();
         firsFifty.forEach(element => {
-        let show = new dataModule.Show(element.name, element.image.medium, element.id, element.summary);
-        
-        tvShows.addShow(show); 
-    });
-    UIModule.makeListOfMovies(tvShows.showsList)
-
-
-
+            let show = new dataModule.Show(element.name, element.image.medium, element.id, element.summary);
+            tvShows.addShow(show);
+        });
+        UIModule.makeListOfMovies(tvShows.showsList);
     })
 
 
-})
+    
+    
+    $(document).on('click', 'a', function (event) {
+     
+     const clickedId = $(this).attr('id');
+     var clickedShow;
+     
+     for (let i = 0; i < tvShows.showsList.length; i++) {
+         if(tvShows.showsList[i].id == clickedId) {
+             clickedShow = tvShows.showsList[i];
+            }
+        }
+        
+        let seasonRequest = $.ajax({
+            url: `https://api.tvmaze.com/shows/${clickedId}/seasons`,
+            method: 'GET'
+        })
+        
+        seasonRequest.done(function(resSeasons){
+            
+            resSeasons.forEach((element) => {
+                var season = new dataModule.Season(element.premiereDate, element.endDate);
+                clickedShow.addSeason(season);
+            })
+        });
+        
+        let castRequest = $.ajax({
+            url: `https://api.tvmaze.com/shows/${clickedId}/cast`,
+            method: 'GET'
+        })
+        
+        castRequest.done(function(resCast){
+            
+            resCast.forEach((element) => {
+                var cast = new dataModule.Cast(element.person.name);
+                clickedShow.addCast(cast);
+            })
+        });
+        
+        UIModule.showMovieInfo(clickedShow);
+        console.log(clickedShow);
+        
+      //  window.location.href = 'showinfoPage.html';
+    });
+    
+
 })(dataModule, UIModule);
